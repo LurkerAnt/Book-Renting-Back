f6d3360cd8cbfdc2baa66444b40d7ebdbcec4c93
Andriy <andsei91@gmail.com>
2020-12-27 14:44:59 +0100

    first commit

---
+++ src/main/java/com/dioni/bookrenting/BookrentingApplication.java
@@ -0,0 +1,13 @@
+package com.dioni.bookrenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrentingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dioni/bookrenting/BookrentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dioni.bookrenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
